2b3b32d89eee118ff1b11024d16fa42f6d7ecc13
k <kkabubenson@gmail.com>
2020-05-20 08:58:12 -0400

    add js elements

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var add = function(number1, number2){return number1 + number2;};
+var number1 = parseInt(prompt("enter a number1: "));
+var number2 = parseInt(prompt("enter a number2: "));
+alert(add(number1, number2));
