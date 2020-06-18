daee7df192c1cb1cc306f9a2aebee52733237fc4
k <kkabubenson@gmail.com>
2020-06-18 11:09:43 -0400

    separate business logic and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-var add = function(number1, number2){return number1 + number2;};
-var number1 = parseInt(prompt("enter a number1: "));
-var number2 = parseInt(prompt("enter a number2: "));
-alert(add(number1, number2));
+
+
+// Business (or back-end) logic:
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+  //everything above this line is business (or back-end) logic:
+//everything below this line is user interface logic(front end)
+$(document).ready(function(){
+$("form#add").submit(function(event){
+    event.preventDefault();
+
+var number1 = parseInt($("#add1").val());
+var number2 = parseInt($("#add2").val());
+var result = add(number1, number2);
+$("#output").text(result);
+});
+});
